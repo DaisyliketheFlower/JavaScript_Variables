12c63e315913469ed59b71310fd5531eb63eb078
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-04-22 05:02:38 -0400

    done

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
-
 // Exercise 2
+const firstName = "Daisy";
+let lastName = 'baby';
+var age = 25;
+
+//Template Literal
+console.log('Hi, my name is  ${firstName} ${lastName} and Iam ${age} years old');
 
 // Exercise 3
+let language = 'Javascript' ;
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('The ${language} language was created in ${createdYear} and it is ${isCaseSensitive}that it is case sensitive.');
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
